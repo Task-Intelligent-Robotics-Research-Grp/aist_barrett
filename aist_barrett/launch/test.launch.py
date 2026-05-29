902000f3e9db6688ae2df1420ab87100f2640707
Toshio UESHIBA <t.ueshiba@aist.go.jp>
2026-05-29 12:04:11 +0900

    Make console wait until its child node terminates

--- aist_barrett/launch/test.launch.py
+++ aist_barrett/launch/test.launch.py
@@ -31,7 +31,7 @@ def launch_setup(context):
         Node(name=['test_client'],
              package='aist_barrett',
              executable=['test_client.py'],
-             prefix=['gnome-terminal --tab --'],
+             prefix=['gnome-terminal --tab --wait --'],
              output='screen'),
         Node(name='rviz', package='rviz2', executable='rviz2',
              output='screen',
